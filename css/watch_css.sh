705c02f708e80a4e629428e0a29e84907b748a41
Jack Cogdill <jackcogdill@users.noreply.github.com>
2017-07-05 00:18:54 -0700

    Stop using inline map from css autoprefixer

--- css/watch_css.sh
+++ css/watch_css.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 sass -t compressed --watch style.scss:_style.css &\
-    find _style.css | entr postcss _style.css --use autoprefixer -o style.css
+    find _style.css | entr postcss _style.css --no-map --use autoprefixer -o style.css
 ps | grep sass | grep -v grep | grep -oE " [0-9]+ $(whoami)" | awk '{print $1}' | xargs kill -9
