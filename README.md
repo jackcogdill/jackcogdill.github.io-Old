6206d43dcc35bb96188b04214b80ed6652d00941
Jack Cogdill <jackcogdill@users.noreply.github.com>
2017-07-14 23:18:49 -0700

    Initial setup

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# shirousagi
-
 ![Love](https://img.shields.io/badge/made%20with-%E2%9D%A4-ff69b4.svg?style=flat-square)
 
 Source code for [jackcogdill.us](http://jackcogdill.us/).
@@ -8,9 +6,3 @@ Source code for [jackcogdill.us](http://jackcogdill.us/).
 
 Written by Jack Cogdill and licensed with [GPLv3](https://www.gnu.org/licenses/gpl.html).  
 SVGs lovingly used from [GitHub](https://octicons.github.com/).
-
-### Todo
-- Organize javascript code
-    - Redo function completions using promises
-    - Use nmp for useful packages?
-    - Use webpack?
--- css/watch_css.sh
+++ css/watch_css.sh
@@ -1,6 +1,22 @@
 #! /bin/bash
 
+sassfile="style.scss"
+intermed="_style.css"
+final="style.css"
+
+# Kill sass processes with Ctrl+C when finished
 trap "ps | grep sass | grep -v grep | grep -oE \"^[0-9]+ \" | awk '{print $1}' | xargs kill -9" SIGINT
-sass -t compressed --watch style.scss:_style.css &\
-    find _style.css | entr postcss _style.css --no-map --use autoprefixer -o style.css
+
+# Ensure intermediate file exists
+if [ ! -f "$intermed" ]; then
+    touch "$intermed"
+fi
+
+sass -t compressed --watch "$sassfile":"$intermed" &\
+    find "$intermed" | entr postcss "$intermed" --no-map --use autoprefixer -o "$final"
+
+# Remove Ctrl-C trap
 trap - SIGINT
+
+# Done with intermediate file
+rm "$intermed"
