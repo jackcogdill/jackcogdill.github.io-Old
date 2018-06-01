353f6769ebdf84d656b3a12b7a23fe07183b1366
Jack Cogdill <jackcogdill@users.noreply.github.com>
2018-06-01 07:05:42 -0400

    Organize files and use gulp to compile

--- css/watch_css.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-trap "ps | grep sass | grep -v grep | grep -oE \"^[0-9]+ \" | awk '{print $1}' | xargs kill -9" SIGINT
-sass -t compressed --watch style.scss:_style.css &\
-    find _style.css | entr postcss _style.css --no-map --use autoprefixer -o style.css
-trap - SIGINT
